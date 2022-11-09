b52c8f98ba502da8c801420ac8df810483bfeb95
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-11-08 23:25:32 -0300

    Adicionado a criptografia nas senhas para os usuarios da migracao seed

--- servidor/seeders/20220727021551-adicionar-usarios.js
+++ servidor/seeders/20220727021551-adicionar-usarios.js
@@ -1,10 +1,23 @@
 'use strict';
 
+const crypto = require('crypto');
+const CHAVE = process.env.CHAVE;
+const IV = process.env.IV;
+const ALGORITMO = process.env.ALGORITMO;
+const METODO_CRIPTOGRAFIA = process.env.METODO_CRIPTOGRAFIA;
+
+const encrypt = ((text) =>  {
+  let cipher = crypto.createCipheriv(ALGORITMO, CHAVE, IV);
+  let encrypted = cipher.update(text, 'utf8', METODO_CRIPTOGRAFIA);
+  encrypted += cipher.final(METODO_CRIPTOGRAFIA);
+  return encrypted;
+});
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('usuarios', [
-      { nome: 'John Doe', usuario:"John" ,senha: 'Infomatica'},
-      { nome: 'Picolo', usuario:"Pic", senha: 'Informatica'},
+      { nome: 'John Doe', usuario:"John" ,senha: encrypt('Infomatica')},
+      { nome: 'Picolo', usuario:"Pic", senha: encrypt('Informatica')},
     ], {});
   },
 
